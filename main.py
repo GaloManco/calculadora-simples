d6665c73d4d2c3f0f48e4a1dd0a45aca368ff1fa
Jonas Franco <minhacontajonas@gmail.com>
2022-07-29 21:14:43 -0300

    Adicionado o input da operação, input dos valores da operação e a operação adicionar.

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 
-#menu 
+# menu
 print('Escolha a operação, \n( + ) Adição \n( - ) Subtração \n( / ) Divisão \n( * ) Multiplicação \n( ** ) Raiz')
 
+# Escolhendo a operação
+print('-'*20)
+operacao = input("Operação: ")
+print('-'*20)
+
+num1 = int(input('Fist number: '))
+num2 = int(input('Second number: '))
+
+if operacao == "+":
+   soma = num1 + num2
+   print(soma) 
